8a8d0a8a07c25e140830b696cc0161055bcbb316
Filip3K <114138650+Filip3Kx@users.noreply.github.com>
2023-09-22 13:52:26 +0200

    add custom node-pool

--- terraform/modules/gke/gke.tf
+++ terraform/modules/gke/gke.tf
@@ -7,17 +7,28 @@ provider "kubernetes" {
     cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
 
-
 module "gke" {
     source = "terraform-google-modules/kubernetes-engine/google"
-    project_id = var.project
+    project_id = ""
     name = "node-app-cluster"
-    region = var.region
-    zones = [ var.zone ]
+    region = "europe-west1"
+    zones = ["europe-west1-b"]
     network = "node-app-vpc"
     subnetwork = "node-app-subnetwork"
     ip_range_pods = "node-app-pods-range"
     ip_range_services = "node-app-services-range"
     node_metadata = "EXPOSE"
     identity_namespace = null
+    node_pools = [
+        {
+            name = "workers"
+            min_count = 2
+            max_count = 5
+            auto_upgrade = true
+            machine_type = "n1-standard-1"
+        }
+    ]
+    node_pools_tags = {
+        all = [ "allow-ssh", "allow-http"]
+    }
 }
